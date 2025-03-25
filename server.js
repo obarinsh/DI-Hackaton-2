6e994c286f71df9d5b4b0e0cc3366e14d56c11e2
obarinshteyn@gmail.com <obarinshteyn@gmail.com>
2025-03-25 15:17:47 +0200

    fixes

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.get('/dashboard', (req, res) => {
 app.use('/api', gameRouter)
 
 const PORT = 3003
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
 })
